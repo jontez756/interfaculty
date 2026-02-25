645c3a73c8f4d2fdbc088cdca8d50fc2611b786a
Your Name <your.email@example.com>
2026-02-25 18:44:54 +0300

    updating

--- interfaculty/urls.py
+++ interfaculty/urls.py
@@ -17,6 +17,9 @@ urlpatterns = [
 
 
 
+
 if settings.DEBUG:
       urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
       urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+      path('faq/', include('faq.urls')),
