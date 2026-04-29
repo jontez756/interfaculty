5ca76de33cc55b501b20111f0b90dd496b6631cc
Your Name <your.email@example.com>
2026-04-29 13:54:21 +0300

    rr

--- interfaculty/settings.py
+++ interfaculty/settings.py
@@ -147,11 +147,13 @@ PASSWORD_RESET_TIMEOUT = 3600  # 1 hour
 FAQ_SETTINGS = ['no_category'] 
 
 
-
+# Email Configuration
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = config('EMAIL_HOST_USER')  # Your Gmail address
-EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')  # Your 16-char app password
-DEFAULT_FROM_EMAIL = 'Inter-Faculty Transfer <noreply@yourdomain.com>'  # Use your email or a
+EMAIL_HOST_USER = 'your-email@gmail.com'
+EMAIL_HOST_PASSWORD = 'your-app-password'
+DEFAULT_FROM_EMAIL = 'Inter-Faculty Transfer <noreply@interfaculty-transfer.onrender.com>'
+
+
